89ce7d02d3a6e9ebcb2cbd068af1a65c14642448
Tom Mitchell <tmitchel@bbn.com>
2020-02-21 13:50:06 -0500

    Clean up test_sequence

--- sbol/test/test_sequence.py
+++ sbol/test/test_sequence.py
@@ -1,34 +1,35 @@
-import unittest
-from sbol.document import *
-from sbol.config import *
 import os
 import sys
+import unittest
+
+import rdflib
+
+import sbol
 
 MODULE_LOCATION = os.path.dirname(os.path.abspath(__file__))
 CRISPR_EXAMPLE = os.path.join(MODULE_LOCATION, 'resources', 'crispr_example.xml')
 
+
 class TestSequence(unittest.TestCase):
 
-    def setUp(self):
-        pass
-
     def testAddSequence(self):
-        test_seq = Sequence("R0010", "ggctgca")
-        doc = Document()
+        test_seq = sbol.Sequence("R0010", "ggctgca")
+        doc = sbol.Document()
         doc.addSequence(test_seq)
         seq = doc.sequences.get("R0010").elements
 
         self.assertEqual(seq, 'ggctgca')
 
     def testRemoveSequence(self):
-        test_seq = Sequence("R0010", "ggctgca")
-        doc = Document()
+        test_seq = sbol.Sequence("R0010", "ggctgca")
+        doc = sbol.Document()
         doc.addSequence(test_seq)
         doc.sequences.remove(0)
-        self.assertRaises(SBOLError, lambda: doc.sequences.get("R0010"))
+        with self.assertRaises(sbol.SBOLError):
+            doc.sequences.get("R0010")
 
     def testSeqDisplayId(self):
-        doc = Document()
+        doc = sbol.Document()
         doc.read(CRISPR_EXAMPLE)
 
         # List of displayIds
@@ -40,9 +41,9 @@ class TestSequence(unittest.TestCase):
         self.assertCountEqual(listseq_read, listseq)
 
     def testSequenceElement(self):
-        setHomespace('http://sbols.org/CRISPR_Example')
-        Config.setOption('sbol_typed_uris', False)
-        doc = Document()
+        sbol.setHomespace('http://sbols.org/CRISPR_Example')
+        sbol.Config.setOption('sbol_typed_uris', False)
+        doc = sbol.Document()
         doc.read(os.path.join(MODULE_LOCATION, 'resources/crispr_example.xml'))
         # Sequence to test against
         seq = ('GCTCCGAATTTCTCGACAGATCTCATGTGATTACGCCAAGCTACGGGCGGAGTACTGTCCTC'
@@ -55,9 +56,9 @@ class TestSequence(unittest.TestCase):
         self.assertEquals(seq_read, seq)
 
     def testUpdateSequenceElement(self):
-        setHomespace('http://sbols.org/CRISPR_Example')
-        Config.setOption('sbol_typed_uris', False)
-        doc = Document()
+        sbol.setHomespace('http://sbols.org/CRISPR_Example')
+        sbol.Config.setOption('sbol_typed_uris', False)
+        doc = sbol.Document()
         doc.read(os.path.join(MODULE_LOCATION, 'resources/crispr_example.xml'))
         # Sequence to test against
         seq = 'AAAAA'
@@ -67,9 +68,9 @@ class TestSequence(unittest.TestCase):
 
     # File I/O Tests
     def testUpdateWrite(self):
-        setHomespace('http://sbols.org/CRISPR_Example')
-        Config.setOption('sbol_typed_uris', False)
-        doc = Document()
+        sbol.setHomespace('http://sbols.org/CRISPR_Example')
+        sbol.Config.setOption('sbol_typed_uris', False)
+        doc = sbol.Document()
         doc.read(os.path.join(MODULE_LOCATION, 'resources/crispr_example.xml'))
         # Sequence to test against
         seq = 'AAAAA'
@@ -77,7 +78,7 @@ class TestSequence(unittest.TestCase):
         # Write to disk
         doc.write('test.xml')
         # Compare
-        doc2 = Document()  # Document to compare for equality
+        doc2 = sbol.Document()  # Document to compare for equality
         doc2.read('test.xml')
         seq_read = doc2.sequences.get('CRP_b_seq').elements
         self.assertEquals(seq_read, seq)
