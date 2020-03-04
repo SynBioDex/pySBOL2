021dde2241ec85883ef55a5121d931e59afa947d
Tom Mitchell <tmitchel@bbn.com>
2020-03-04 12:28:45 -0500

    Reduce wildcard import limit

--- sbol/test/test_style.py
+++ sbol/test/test_style.py
@@ -8,7 +8,7 @@ MODULE_LOCATION = os.path.dirname(os.path.abspath(__file__))
 SBOL_PATH = os.path.dirname(MODULE_LOCATION)
 
 # Please don't increase this number!
-MAX_WILDCARD_IMPORTS = 7
+MAX_WILDCARD_IMPORTS = 6
 
 
 class TestStyle(unittest.TestCase):
