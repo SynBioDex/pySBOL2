e79af0d8b336326d90c9785a2cf050b27abb8ff7
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-04-28 16:52:14 -0400

    Remove wildcard import

--- test/test_config.py
+++ test/test_config.py
@@ -2,7 +2,7 @@ import unittest
 
 import rdflib
 
-from sbol2 import *
+import sbol2
 
 CRISPR_URI = 'http://sbols.org/CRISPR_Example/CRISPR_Template'
 CRISPR_URI_1 = 'http://sbols.org/CRISPR_Example/CRISPR_Template/1'
@@ -13,27 +13,27 @@ CRISPR_CD_URI_1 = 'http://sbols.org/CRISPR_Example/ComponentDefinition/CRISPR_Te
 class TestConfig(unittest.TestCase):
 
     def test_setOption_invalidOption(self):
-        with self.assertRaises(SBOLError):
-            Config.setOption('invalid_option', 2)
+        with self.assertRaises(sbol2.SBOLError):
+            sbol2.Config.setOption('invalid_option', 2)
 
     def test_setOption_invalidOptionVal(self):
-        with self.assertRaises(SBOLError):
-            Config.setOption(ConfigOptions.VERBOSE.value, 'Yellow')
+        with self.assertRaises(sbol2.SBOLError):
+            sbol2.Config.setOption(sbol2.ConfigOptions.VERBOSE.value, 'Yellow')
 
     def test_hasHomespace_True(self):
-        setHomespace('http://sbols.org/CRISPR_Example/')
-        self.assertTrue(hasHomespace())
+        sbol2.setHomespace('http://sbols.org/CRISPR_Example/')
+        self.assertTrue(sbol2.hasHomespace())
 
     def test_hasHomespace_False(self):
-        setHomespace('')
-        self.assertFalse(hasHomespace())
+        sbol2.setHomespace('')
+        self.assertFalse(sbol2.hasHomespace())
 
     def test_openworld_noHomespace(self):
         """See: https://pysbol2.readthedocs.io/en/latest/getting_started.html"""
-        setHomespace('')
-        Config.setOption('sbol_compliant_uris', False)
-        Config.setOption('sbol_typed_uris', False)
-        crispr_template = ModuleDefinition(CRISPR_URI)
+        sbol2.setHomespace('')
+        sbol2.Config.setOption('sbol_compliant_uris', False)
+        sbol2.Config.setOption('sbol_typed_uris', False)
+        crispr_template = sbol2.ModuleDefinition(CRISPR_URI)
         self.assertEqual(CRISPR_URI, str(crispr_template))
         self.assertEqual(rdflib.URIRef(CRISPR_URI), crispr_template.identity)
 
@@ -42,33 +42,33 @@ class TestConfig(unittest.TestCase):
         # Note: In tutorial CRISPR_Example is followed by a trailing slash.
         # This results in two slashes instead of one in both the original pySBOL
         # and the new code.
-        setHomespace('http://sbols.org/CRISPR_Example')
-        Config.setOption('sbol_compliant_uris', False)
-        Config.setOption('sbol_typed_uris', False)
-        crispr_template = ModuleDefinition('CRISPR_Template')
+        sbol2.setHomespace('http://sbols.org/CRISPR_Example')
+        sbol2.Config.setOption('sbol_compliant_uris', False)
+        sbol2.Config.setOption('sbol_typed_uris', False)
+        crispr_template = sbol2.ModuleDefinition('CRISPR_Template')
         self.assertEqual(CRISPR_URI, str(crispr_template))
         self.assertEqual(rdflib.URIRef(CRISPR_URI), crispr_template.identity)
 
     def test_SBOLCompliant(self):
         """See: https://pysbol2.readthedocs.io/en/latest/getting_started.html"""
-        setHomespace('http://sbols.org/CRISPR_Example')
-        Config.setOption('sbol_compliant_uris', True)
-        Config.setOption('sbol_typed_uris', False)
-        crispr_template = ModuleDefinition('CRISPR_Template')
+        sbol2.setHomespace('http://sbols.org/CRISPR_Example')
+        sbol2.Config.setOption('sbol_compliant_uris', True)
+        sbol2.Config.setOption('sbol_typed_uris', False)
+        crispr_template = sbol2.ModuleDefinition('CRISPR_Template')
         self.assertEqual(CRISPR_URI_1, str(crispr_template))
         self.assertEqual(rdflib.URIRef(CRISPR_URI_1), crispr_template.identity)
 
     def test_SBOLCompliant_typed(self):
         """See: https://pysbol2.readthedocs.io/en/latest/getting_started.html"""
-        setHomespace('http://sbols.org/CRISPR_Example')
-        Config.setOption('sbol_compliant_uris', True)
-        Config.setOption('sbol_typed_uris', True)
-        crispr_template_md = ModuleDefinition('CRISPR_Template')
+        sbol2.setHomespace('http://sbols.org/CRISPR_Example')
+        sbol2.Config.setOption('sbol_compliant_uris', True)
+        sbol2.Config.setOption('sbol_typed_uris', True)
+        crispr_template_md = sbol2.ModuleDefinition('CRISPR_Template')
         expected_crispr_template_md = CRISPR_MD_URI_1
         self.assertEqual(expected_crispr_template_md, str(crispr_template_md))
         self.assertEqual(rdflib.URIRef(expected_crispr_template_md),
                          crispr_template_md.identity)
-        crispr_template_cd = ComponentDefinition('CRISPR_Template')
+        crispr_template_cd = sbol2.ComponentDefinition('CRISPR_Template')
         expected_crispr_template_cd = CRISPR_CD_URI_1
         self.assertEqual(expected_crispr_template_cd, str(crispr_template_cd))
         self.assertEqual(rdflib.URIRef(expected_crispr_template_cd),
