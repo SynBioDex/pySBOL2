6d3a2e3c2a6de05300f2c6038b3224255b014624
Tom Mitchell <tcmitchell@users.noreply.github.com>
2020-05-05 13:59:54 -0400

    Add Document.addAttachment()
    Also fix up the Attachment constructor so that
    a URI argument does not overwrite the type_uri.

--- sbol2/attachment.py
+++ sbol2/attachment.py
@@ -1,13 +1,13 @@
 from .toplevel import TopLevel
 from .constants import *
-from rdflib import URIRef
 from .property import URIProperty, LiteralProperty
 
 
 class Attachment(TopLevel):
 
-    def __init__(self, type_uri=SBOL_ATTACHMENT, uri=URIRef("example"),
-                 version=VERSION_STRING, source=''):
+    def __init__(self, uri=URIRef("example"),
+                 *, type_uri=SBOL_ATTACHMENT, version=VERSION_STRING,
+                 source=''):
         super().__init__(type_uri, uri, version)
         self._source = URIProperty(self, SBOL_SOURCE, '1', '1', [], source)
         self._format = LiteralProperty(self, SBOL_URI + '#format', '0', '1', [])
