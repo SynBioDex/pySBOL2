f580855b9bab971098ff0c8535d3749dc125e743
Tom Mitchell <tcmitchell@users.noreply.github.com>
2022-04-14 09:50:28 -0400

    Prep release 1.4.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='sbol2',
-      version='1.5',
+      version='1.4.1',
       description='Pure Python implementation of SBOL 2 standard',
       python_requires='>=3.7',
       url='https://github.com/SynBioDex/pySBOL2',
