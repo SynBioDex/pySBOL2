1347d3c1a3c2014935a6bf2d3876edc1abdf9776
Tom Mitchell <tcmitchell@users.noreply.github.com>
2022-04-14 10:10:15 -0400

    Bump version to 1.5

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Raytheon BBN Technologies'
 author = 'Bryan Bartley and Tom Mitchell'
 
 # The full version, including alpha/beta/rc tags
-release = '1.4.1'
+release = '1.5'
 
 
 # -- General configuration ---------------------------------------------------
